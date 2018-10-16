dfb1f9edea5a5f3c64f1d68164f6d5222c52f8b3
noreakram21 <noreakram22@gmail.com>
2018-10-16 18:22:26 +0800

    Add get todos

--- server/server.js
+++ server/server.js
@@ -18,7 +18,13 @@ res.status(400).send(e);
 });
 });
 
-
+app.get('/todos', (req, res)=>{
+	Todo.find().then((todos)=>{
+		res.send({todos});
+	},(e) =>{	
+		res.status(400).send(e);
+	});
+});
 app.listen(3000,() =>{
 	console.log('started on port 3000')
 });
