7192d59513c5e36e484b2eeea696e32784d670ff
noreakram21 <noreakram22@gmail.com>
2018-10-17 11:31:18 +0800

    getting id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
-
+let {ObjectID} = require('mongodb');
 let {mongoose} =  require('./db/mongoose');
 let {Todo} =  require('./model/todo');	
 
@@ -25,6 +25,26 @@ app.get('/todos', (req, res)=>{
 		res.status(400).send(e);
 	});
 });
+
+app.get('/todos/:id', (req,res) => {
+	let id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+	Todo.findById(id).then((todo)=>{
+		if(!Todo){
+			return res.status(404).send();
+		}
+
+		res.send({todo});
+
+	}).catch((e)=>{
+		res.status(400).send();
+	})
+
+});
+
+
 app.listen(3000,() =>{
 	console.log('started on port 3000')
 });
